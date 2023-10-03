fc27d1f8caa710f627c2a54a8ef2626709a85ca7
BayajidAlam <bayajidalam2001@gmail.com>
2023-10-03 10:20:04 +0600

    first module done

---
+++ basic-styntex-variable-data-type/limitations.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{ 
+  // 10 digit for int 
+  int a=1000000000;
+  // 20 for lli 
+  long long int b=100000000000;
+  printf("%lld",b);
+}
---
+++ basic-styntex-variable-data-type/variable.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+  // int 
+  int rahim = 100, karim=200;
+  // printf("%d %d",rahim, karim);
+
+  // float 
+  float chol = 2.5;
+  //printf("%0.4f",chol);
+
+  // char 
+  char ami = 'k';
+  printf("%c",ami);
+}
