d2c3e08c621ba845b6d7aa07fd0f23ef2e5e8b26
BayajidAlam <bayajidalam2001@gmail.com>
2023-10-08 19:40:46 +0600

    loop done

--- loop/continue.c
+++ loop/continue.c
@@ -5,11 +5,11 @@ int main()
   int n;
   scanf("%d", &n);
   for(int i=0; i<=n; i += 1){
-    printf("%d\n", i);
-    // immediately terminate the loop 
+    // skip the match condition 
     if(i == 5 ) {
-      break;
+      continue;
     }
+    printf("%d\n", i);
   }
   return 0;
 }
