e66ceb9581635dc67032876a05b78f3d615106fe
BayajidAlam <bayajidalam2001@gmail.com>
2023-10-28 21:29:04 +0600

    frequency array

---
+++ string-operation_and_frequency-arry/frequency_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+
+  int n;
+  scanf("%d", &n);
+  int a[n];
+  for (int i = 0; i < n; i++)
+  {
+    scanf("%d", &a[i]);
+  }
+  int cnt[9] = {0};
+  for (int i = 0; i < n; i++)
+  {
+    int value = a[i];
+    cnt[value]++;
+  }
+  for (int i = 0; i < 9; i++)
+  {
+    printf("%d - %d\n", i, cnt[i]);
+  }
+  return 0;
+}
